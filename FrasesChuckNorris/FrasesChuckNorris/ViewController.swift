881e0b94e791d84cbb713a67e2dc8715cfcd15ef
andre dias <andredias@Mac-mini-de-andre.local>
2018-12-04 00:24:50 -0200

    Commit inicial

---
+++ FrasesChuckNorris/FrasesChuckNorris/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FrasesChuckNorris
+//
+//  Created by andre dias on 03/12/18.
+//  Copyright © 2018 andre dias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FrasesChuckNorris/Pods/Target Support Files/Pods-FrasesChuckNorrisUITests/Pods-FrasesChuckNorrisUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FrasesChuckNorrisUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FrasesChuckNorrisUITests
+@end
